2eb3eb8f39c9b7231ccc933e5f8d5ddae59ecb4f
shraddhadhyade <shraddhadhyade@gmail.com>
2021-04-04 16:50:34 -0700

    Complete project

---
+++ TestApp/src/app/starttest/starttest.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-starttest',
+  templateUrl: './starttest.component.html',
+  styleUrls: ['./starttest.component.css']
+})
+export class StarttestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // startTest(){
+  //   this.router.navigate(["test"]);
+  // }
+}
+
